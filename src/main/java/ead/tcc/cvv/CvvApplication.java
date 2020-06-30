10dfa4d646ed80974072eae90e7a72f52e2267b4
Renato Perdiz Rodrigues <renato@proxy.com.br>
2020-06-30 19:04:27 -0300

    push inicial

---
+++ src/main/java/ead/tcc/cvv/CvvApplication.java
@@ -0,0 +1,13 @@
+package ead.tcc.cvv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ead/tcc/cvv/CvvApplicationTests.java
@@ -0,0 +1,13 @@
+package ead.tcc.cvv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
